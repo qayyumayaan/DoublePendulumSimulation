293f4d8ffead951bbadf98a9d9ef7fabb0a6a2c3
qayyumayaan <96453808+qayyumayaan@users.noreply.github.com>
2023-05-13 18:19:23 -0400

    Add a reset button and timestep

--- script.js
+++ script.js
@@ -14,6 +14,8 @@ let length1 = 200; // length of the first pendulum
 let length2 = 200; // length of the second pendulum
 let mass1 = 20; // mass of the first pendulum
 let mass2 = 20; // mass of the second pendulum
+let simulationSpeed = 1;
+let timeStep = .1
 
 const resetButton = document.getElementById('resetButton');
 resetButton.addEventListener('click', resetSimulation);
@@ -86,6 +88,7 @@ function drawPendulum(x, y, angle, length, mass) {
 }
 
 function update() {
+
   const numerator1 =
     -g * (2 * mass1 + mass2) * Math.sin(theta1) -
     mass2 * g * Math.sin(theta1 - 2 * theta2) -
@@ -103,13 +106,13 @@ function update() {
       omega2 * omega2 * length2 * mass2 * Math.cos(theta1 - theta2));
   const denominator2 = length2 * (2 * mass1 + mass2 - mass2 * Math.cos(2 * theta1 - 2 * theta2));
 
-  const alpha1 = numerator1 / denominator1;
-  const alpha2 = numerator2 / denominator2;
+  const alpha1 = simulationSpeed * (numerator1 / denominator1);
+  const alpha2 = simulationSpeed * (numerator2 / denominator2);
 
-  omega1 += alpha1;
-  omega2 += alpha2;
-  theta1 += omega1;
-  theta2 += omega2;
+  omega1 += alpha1 * timeStep;
+  omega2 += alpha2 * timeStep;
+  theta1 += omega1 * timeStep;
+  theta2 += omega2 * timeStep;
 
   ctx.clearRect(0, 0, width, height);
   const pivotX = width / 2;
@@ -121,6 +124,27 @@ function update() {
 
   requestAnimationFrame(update);
 }
+
+function resetSimulation() {
+  // Reset the pendulum angles and angular velocities to their initial values
+  theta1 = Math.PI / 2;
+  theta2 = Math.PI / 2;
+  omega1 = 0;
+  omega2 = 0;
+
+  // Update the simulation values based on the current slider values
+  length1 = Number(document.getElementById('length1Slider').value);
+  length2 = Number(document.getElementById('length2Slider').value);
+  mass1 = Number(document.getElementById('mass1Slider').value);
+  mass2 = Number(document.getElementById('mass2Slider').value);
+
+  // Clear the canvas
+  ctx.clearRect(0, 0, width, height);
+
+  // Reset the simulation with the updated values
+  update();
+}
+
 
 // Utility function to convert degrees to radians
 function degToRad(degrees) {
